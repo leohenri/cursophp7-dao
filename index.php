6b12984e439521c5fc27712d6f04659327440dcf
Leandro Henrique Barbosa <leohenribar@gmail.com>
2022-02-27 15:12:51 -0300

    Metodos de listas da class Usuarios

--- index.php
+++ index.php
@@ -10,9 +10,21 @@ $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 echo json_encode($usuarios);
 */
 
-$root = new Usuarios();
+//$root = new Usuarios();
+//Carrega apenas um usuario especifico
+//$root->loadById(4);
 
-$root->loadById(4);
+//Carrega lista de usuarios
+//$lista = Usuarios::getList();
+//echo json_encode($lista);
 
-echo $root;
+
+//Carrega login espcifico
+//$lista = Usuarios::search("ro");
+//echo json_encode($lista);
+
+//Carrega um usuario
+$usuario = new Usuarios();
+$usuario->login('userRrRrRr', '102030');
+echo ($usuario);
 ?>
