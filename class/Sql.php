8dda71c86d8b571d35c8f42716e30b9e7adc08e7
Leandro Henrique Barbosa <leohenribar@gmail.com>
2022-02-27 01:19:53 -0300

    Carregamento da classe Usuarios

--- class/Sql.php
+++ class/Sql.php
@@ -29,14 +29,14 @@ class Sql extends PDO{
 
 	}
 
-	private function setParams($statment, $parameters = array()){
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-			$this->setParam($statment, $key, $value);
+			$this->setParam($statement, $key, $value);
 		}
 	}
 
-	private function setParam($statment, $key, $value){echo 'entrei';
-		$statment->bindParam($key, $value);
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
 	}
 
 	public function query($rawQuery, $params = array()){
